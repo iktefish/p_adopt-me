b631efb205e81e97a32616f625a4621e43fc57db
Ivan Shafiuddin <87042742+iktefish@users.noreply.github.com>
2021-11-27 16:44:44 +0600

    Using Props in Components

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
-const Pet = () => {
-    return React.createElement(
-        "div",
-        {},
+const Pet = (props) => {
+    return React.createElement("div", {},
         [
-            React.createElement("h2", {}, "Yuna"),
-            React.createElement("h3", {}, "Dog"),
-            React.createElement("h3", {}, "Pariah"),
+            React.createElement("h2", {}, props.name),
+            React.createElement("h3", {}, props.animal),
+            React.createElement("h3", {}, props.breed),
         ]
     );
 };
@@ -17,9 +15,9 @@ const App = () => {
         [
             React.createElement("h1", {}, "Adope Me"),
             ...[1, 2, 3, 4].map((i) => React.createElement("h1", {}, i)),
-            React.createElement(Pet),
-            React.createElement(Pet),
-            React.createElement(Pet),
+            React.createElement(Pet, { name: "Yuna", animal: "Dog", breed: "Pariah" }),
+            React.createElement(Pet, { name: "Lorna", animal: "Bird", breed: "Cockatiel" }),
+            React.createElement(Pet, { name: "Iktefish", animal: "Fish", breed: "Gentlefish" }),
         ]
     );
 };
